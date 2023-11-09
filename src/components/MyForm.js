81ad5399c18b21486522e63a186df31bbe73a8ec
jay soni <jaysoni1385@gmail.com>
2023-11-09 16:53:24 +0530

    v 1.5

--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import { MdClose } from 'react-icons/md'
 
-const MyForm = ({handelSubmit,handelOnChange,handelClose,rest,heading}) => {
+const MyForm = ({ handelSubmit, handelOnChange, handelClose, rest, heading }) => {
   return (
     <div className="addContainer">
-              <form onSubmit={handelSubmit}>
-                <h2>{heading}</h2>
-                <div className="close-btn" ><MdClose onClick={handelClose} /></div>
-                <label htmlFor="name" >Name :</label>
-                <input onChange={handelOnChange} type="text" placeholder='Enter Name' name="name" id="name" value={rest.name} />
-                <label htmlFor="email" >Email :</label>
-                <input onChange={handelOnChange} type="email" placeholder='Enter Email' name="email" id="email" value={rest.email}/>
-                <label htmlFor="mobile" >Mobile :</label>
-                <input onChange={handelOnChange} type="tel" placeholder='Enter Mobile No.' name="mobile" id="mobile" value={rest.mobile} />
-                <button type='submit' className='btn'>{heading==="Update User"?"Update":"Add"}</button>
-              </form>
-            </div>
+      <form onSubmit={handelSubmit}>
+        <h2>{heading}</h2>
+        <div className="close-btn" ><MdClose onClick={handelClose} /></div>
+        <label htmlFor="name" >Name :</label>
+        <input onChange={handelOnChange} type="text" placeholder='Enter Name' name="name" id="name" value={rest.name} />
+        <label htmlFor="email" >Email :</label>
+        <input onChange={handelOnChange} type="email" placeholder='Enter Email' name="email" id="email" value={rest.email} />
+        <label htmlFor="mobile" >Mobile :</label>
+        <input onChange={handelOnChange} type="tel" placeholder='Enter Mobile No.' name="mobile" id="mobile" value={rest.mobile} />
+        <button type='submit' className='btn'>{heading === "Update User" ? "Update" : "Add"}</button>
+      </form>
+    </div>
   )
 }
 
